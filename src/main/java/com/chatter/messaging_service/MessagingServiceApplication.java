73ed7d3a8c3a64ae16d8298f7adff1ea63143b4c
Maxim Serov <maxim58r@gmail.com>
2024-12-20 22:02:08 +0300

    commit checkstyle

--- src/main/java/com/chatter/messaging_service/MessagingServiceApplication.java
+++ src/main/java/com/chatter/messaging_service/MessagingServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MessagingServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MessagingServiceApplication.class, args);
-	}
+    /**
+     * Entry point for the authentication service application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingServiceApplication.class, args);
+    }
 
 }
